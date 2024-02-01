ed91d24e5d4811f84c34ccbfdb1f246a4d3a0b16
Ranna <jran0323@gmail.com>
2024-02-01 17:51:39 +0900

    feat: 하위 도메인 라우팅
    - 하위 도메인 라우팅 api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpCode, Redirect } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    // const { name, email } = createUserDto;
+
+    return `유저를 생성했습니다. 이름: `;
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Res() res) {
+    const users = this.usersService.findAll()
+
+    return res.status(200).send(users);
   }
 
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   if (+id < 1) {
+  //     throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+  //   }
+
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Header('Custom', 'Test Header')
+  // @Get(':id')
+  // findOneWithHeader(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  @Redirect('https://nestjs.com', 301)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @HttpCode(202)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
@@ -31,4 +52,18 @@ export class UsersController {
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
+
+  // @Delete(':userId/memo/:memoId')
+  // deleteUserMemo(@Param() params: { [key: string]: string }) {
+  //   return `userId: ${params.userId}, memoId: ${params.memoId}`;
+  // }
+
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo(
+      @Param('userId') userId: string,
+      @Param('memoId') memoId: string,
+  ) {
+    return `userId: ${userId}, memoId: ${memoId}`;
+  }
+
 }
