b2e7e97496e63674a902b63c69f85e8f970f8ed0
Ranna <jran0323@gmail.com>
2024-02-28 17:21:19 +0900

    feat: user create db connect
    - user create 부분 db connect
    - dto, module과 prisma 연결

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from "src/prisma.service";
+import { Injectable, HttpException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService){}
+  async createUser(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: createUserDto
+    })
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  // findAll() {
+  //   return `This action returns all users`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const found = await this.prisma.user.findUnique({where: {id:Number(id)}})
+    if(!found){
+      throw new HttpException(`there is no ${id}`, 400);
+    }
+    const { password, id: userId, ...result } = found;
+    return result;
+    // return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
